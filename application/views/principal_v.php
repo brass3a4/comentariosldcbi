9434e54b2664351bbacf2b9c8b82a11badfbdedd
brass3a4 <brass3a4@gmail.com>
2012-12-21 12:42:39 -0600

    tabs

--- application/views/principal_v.php
+++ application/views/principal_v.php
@@ -8,7 +8,10 @@
 <meta name="viewport" content="width=device-width" />
 
 <link rel="stylesheet" href="<?=base_url(); ?>statics/css/core/foundation.min.css">
-<link rel="stylesheet" href="<?=base_url(); ?>statics/js/fundation/foundation.min.js">
+<script src="<?=base_url(); ?>statics/js/fundation/foundation.min.js"></script>
+<script src="<?=base_url(); ?>statics/js/foundation/modernizr.foundation.js" ></script>
+<script src="<?=base_url(); ?>statics/js/foundation/app.js" ></script>
+<script src="<?=base_url(); ?>statics/js/foundation/*.js" ></script>
 <link rel="stylesheet" href="<?=base_url(); ?>statics/css/comentario.css">
 	</head>
 
@@ -19,10 +22,39 @@
 			
 			<div class="eight columns titulo">
 				Comentarios y sugerencias hacia los Laboratorios de Docencia CBI			
-	  			
+			</div>
+			<div class="two columns"></div>
+			
+		</div>
+		
+		<div class="twelve columns espacioSuperior">
+			<div class="two columns"></div>
+			
+			<div class="eight columns">
+				
+				
+
+
+<dl class="tabs contained">
+  <dt>Title 1</dt>
+  <dd class="active"><a href="#simpleContained1">Simple Tab 1</a></dd>
+  <dd class="hide-for-small"><a href="#simpleContained2">Simple Tab 2</a></dd>
+  <dt class="hide-for-small">Title 1</dt>
+  <dd class="hide-for-small"><a href="#simpleContained3">Simple Tab 3</a></dd>
+</dl>
+<ul class="tabs-content contained">
+  <li class="active" id="simpleContained1Tab">This is simple tab 1s content. Pretty neat, huh?</li>
+  <li id="simpleContained2Tab">This is simple tab 2s content. Now you see it!</li>
+  <li id="simpleContained3Tab">This is simple tab 3s content.</li>
+</ul>
+
+
+
+				
 			</div>
 			
 			<div class="two columns"></div>
+			
 		</div>
 	
 	</body>
